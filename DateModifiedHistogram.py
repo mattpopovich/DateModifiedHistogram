0bf88f4eea3b5b22f6f15639c2523332964a6702
mattpopovich <popovich.matt@gmail.com>
2019-12-25 09:48:07 -0500

    Histograms are being generated

--- DateModifiedHistogram.py
+++ DateModifiedHistogram.py
@@ -9,14 +9,15 @@
 import os.path
 import time
 import datetime 
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 # --- Functions ---
 # Returns epoch time the file_path was last modified 
 def parse_file(file_path):
-	# print("Parsing file: " + str(file_path))
 	file_epoch = os.path.getmtime(file_path)
+	# print("Parsing file: " + str(file_path) + " with epoch: " + str(file_epoch))
 	return file_epoch
 
 
@@ -31,6 +32,9 @@ def parse_folder(data_dir):
 
 
 	for file_name in os.listdir(data_dir):
+		if file_name[0] == '.':
+			print("Skipping file: " + str(file_name))
+			continue # Skip processing this file
 		abs_path = data_dir + '/' + file_name
 
 		# print("Parsing file/folder: " + str(abs_path))
@@ -47,16 +51,48 @@ def parse_folder(data_dir):
 
 		new_dir = False
 
-	#generate_histogram(data_dir, folder_times)
+	generate_histogram(data_dir, folder_times)
 	print("folder_times for folder '" + data_dir + "' = " + str(len(folder_times)))
-	print("folder_times values: " + str(folder_times))
+	# print("folder_times values: " + str(folder_times))
 	return folder_times
 
 
 def generate_histogram(data_dir, folder_times):
 	folder_name = os.path.basename(data_dir)
 
-	datetime.datetime.fromtimestamp(os.path.getmtime(var)).strftime('%Y-%m-%d %H:%M:%S')
+	file_seconds = []
+
+	for t in folder_times:
+		# print("t: " + str(t))
+		H = int(datetime.datetime.fromtimestamp(t).strftime('%H'))
+		M = int(datetime.datetime.fromtimestamp(t).strftime('%M'))
+		S = int(datetime.datetime.fromtimestamp(t).strftime('%S'))
+		# print("H: {}, M: {}, S: {}".format(H, M, S))
+		total_minutes = (H * 60) + M
+		total_seconds = (total_minutes * 60) + S
+		file_seconds.append(total_seconds)
+
+	sec_in_hour = 60 * 60
+	sec_in_day = sec_in_hour * 24
+
+	# print("file_seconds: " + str(file_seconds))
+
+	file_hours = np.array(file_seconds) / sec_in_hour
+
+	print("file_hours: " + str(file_hours))
+
+	num_bins = 23
+	n, bins, patches = plt.hist(file_hours, num_bins)
+	print("n: " + str(n))
+	print("len n: " + str(len(n)))
+	print("bins: " + str(bins))
+	print("len bins: " + str(len(bins)))
+	print("patches: " + str(patches))
+	plt.xlabel('Hour of the day')
+	plt.ylabel('Occurrences')
+	plt.title(folder_name)
+	plt.xticks(np.arange(0, 25, 1))
+	plt.show()
 
 
 
@@ -64,8 +100,9 @@ def generate_histogram(data_dir, folder_times):
 
 
 # --- Declarations --- 
-dataDir = '/Volumes/FAT32_4/2019 TrailCam/test'
-
+# dataDir = '/Volumes/FAT32_4/2019 TrailCam/test'
+# dataDir = '/Users/mattpopovich/Desktop/2019 TrailCam/test'
+dataDir = '/Users/mattpopovich/Desktop/2019 TrailCam'
 
 
 # --- Main Code ---
